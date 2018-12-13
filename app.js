e7f1574cc69a8c40217b06828a27e1d21bf6cd61
Daniel Díaz <dalejodc@gmail.com>
2018-12-12 19:22:21 -0600

    Using HBS partials

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
-var express = require('express')
-var app = express()
- 
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+
+//To make public a dir
+app.use(express.static(__dirname + '/public'));
+
+//Using HBS as the default view engine
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.render('home', {
+    section: 'Home'
+  });
 })
- 
-app.listen(3000, () =>{
-    console.log('Running in the port 3000');
+
+app.listen(3000, () => {
+  console.log('Running in the port 3000');
 })
