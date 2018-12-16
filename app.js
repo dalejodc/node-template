f1aba4dfbb2a2cfaf7c92b5b3158114e838ee77a
Daniel Díaz <dalejodc@gmail.com>
2018-12-15 21:58:01 -0600

    Some styles, getting the year from the server

--- app.js
+++ app.js
@@ -12,7 +12,10 @@ hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
 
 app.get('/', function (req, res) {
-  res.render('home', {title: 'Home'});
+  res.render('home', {
+    title: 'Home',
+    hi: new Date().getFullYear()
+  });
 })
 
 console.log(PORT);
